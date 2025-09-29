59a5ce947343d617dbebec5596e8e80e9b3cdf9e
Abhishek <24BAI80009@cuchd.in>
2025-09-29 07:16:56 +0000

    commit

---
+++ lab_mst/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StudentManagement from "./StudentManagement"; // Make sure this path is correct
+
+const App = () => {
+  return (
+    <div>
+      <StudentManagement />
+    </div>
+  );
+};
+
+export default App;
